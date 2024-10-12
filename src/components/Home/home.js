e254aa8a5dd19a4a16a1e98b7eff17ed458165c7
Emmy Gardner <emmygardner@Emmys-Air.lan>
2024-10-12 13:34:55 -0700

    basic home page layout

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -3,7 +3,20 @@ import './home.css';
 export default function Home(){
     return(
         <div className='container'>
-            Yay!!!
+            <div className='homeContent'>
+                <div className='homeHeadshot'>
+
+                </div>
+                <div className='homeText'>
+                    <p>
+                        hi, I'm Emmy!
+                    </p>
+                    <p>
+                        here's some stuff about me...
+                    </p>
+                    <p>contact me!</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,5 +1,10 @@
 import './projects.css';
 
+// Is Mommy Okay?
+// Strip the Stigma
+// Big Joe's Dent Removal commercial
+// IMTS West
+
 export default function Projects(){
     return(
         <div className='container'>
