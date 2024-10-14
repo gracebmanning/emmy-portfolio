a74c201a3eab85e3aacfeceead589142eff41739
Grace Manning <gracebmanning@gmail.com>
2024-10-13 19:14:06 -0700

    update navbar with resume

--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -1,7 +1,7 @@
 import './navigation.css';
 import { MdLocalMovies } from "react-icons/md";
 import { BiSolidCameraMovie } from "react-icons/bi";
-
+import { IoDocumentText } from "react-icons/io5";
 
 export default function Navigation(){
     return(
@@ -18,6 +18,12 @@ export default function Navigation(){
                     <a href="/projects">
                         <BiSolidCameraMovie className='navIcon' />
                         projects
+                    </a>
+                </li>
+                <li className='navItem'>
+                    <a href="/resume" target="_blank">
+                        <IoDocumentText className='navIcon' />
+                        resumé
                     </a>
                 </li>
             </ul>
