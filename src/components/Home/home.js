257297a1768429a645176cacfa489f12acef54c2
Grace Manning <gracebmanning@gmail.com>
2024-10-13 19:18:45 -0700

    update home page bio

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -8,12 +8,21 @@ export default function Home(){
                 <img className='homeHeadshot' src={headshot} alt='Emmy holding a slate at a production' />
                 <div className='homeText'>
                     <p>
-                        hi, I'm Emmy!
+                        hi, I'm <b>Emmy</b>!
                     </p>
                     <p>
-                        welcome to my website, where you'll find examples of some projects I've worked on. Thanks for visiting!
+                        welcome to my website. 
+                        i am aspiring in the field of <b>digital and traditional media production</b> with a particular interest in <b>screenwriting</b> and <b>producing</b>. 
+                        i have spent the past year working as a production assistant and operating cameras under several different production companies. 
+                        if you click on the <a id='projectsLink' href='/projects'>projects</a> tab of this website, you'll find examples of some projects i've worked on. 
+                        thanks for visiting!
                     </p>
-                    <p>contact me at emmythegardner [@] gmail.com</p>
+                    <p>
+                        outside of my work, i am also an avid media consumer myself. i love to spend time with my cat, hit up the local vintage, thrift, and oddities shops, and go hiking around LA. i have worked at several zoos and am passionate about animal care and environmental conservation. i will do just about anything for a can of diet coke. 
+                    </p>
+                    <p>
+                        you can contact me with any inquiries at emmythegardner [@] gmail.com
+                    </p>
                 </div>
             </div>
         </div>
