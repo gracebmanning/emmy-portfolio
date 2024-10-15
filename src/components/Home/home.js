e78995ec8bbcf65c1cc001dbe5e89271d2764896
Grace Manning <gracebmanning@gmail.com>
2024-10-14 20:03:01 -0700

    home page film strip

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,9 +1,12 @@
 import './home.css';
 import headshot from '../../assets/images/headshot.jpg';
+import emmyAndMoose from '../../assets/images/emmy-and-moose.png';
+import bigJoesFrame1 from '../../assets/images/bigjoes-frame1.jpg';
+import bigJoesFrame2 from '../../assets/images/bigjoes-frame2.jpg';
 
 export default function Home(){
     return(
-        <div className='container'>
+        <div className='container homeContainer'>
             <div className='homeContent'>
                 <img className='homeHeadshot' src={headshot} alt='Emmy holding a slate at a production' />
                 <div className='homeText'>
@@ -25,6 +28,22 @@ export default function Home(){
                     <p>
                         you can contact me with any inquiries at emmythegardner [@] gmail.com
                     </p>
+                </div>
+            </div>
+            <div className='homePhotoGallery filmStrip'>
+                <div className='scroll'>
+                    <div className='filmStripPhoto'>
+                        <img src={emmyAndMoose} alt='Emmy sitting with Moose, her orange cat, in her lap while working at her laptop.' />
+                        <p>my diligent assistant Moose and i hard at work on this very website</p>
+                    </div>
+                    <div className='filmStripPhoto'>
+                        <img src={bigJoesFrame1} alt='Emmy puts her hand up to her, pausing in anticipation.' />
+                        <p>wait...</p>
+                    </div>
+                    <div className='filmStripPhoto'>
+                        <img src={bigJoesFrame2} alt='Emmy pulls down her sunglasses, looking shocked.' />
+                        <p>woah!</p>
+                    </div>
                 </div>
             </div>
         </div>
