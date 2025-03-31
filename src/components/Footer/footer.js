59ed24fba6e1e5b461f358d59609dd72d5b79a11
Grace Manning <gracebmanning@gmail.com>
2025-03-30 18:05:17 -0700

    switch to Nextjs with Sanity

--- src/components/Footer/footer.js
+++
@@ -1,9 +0,0 @@
-import './footer.css';
-
-export default function Footer(){
-    return(
-        <footer>
-            site created by&nbsp;<a href='https://grace-manning.com/' target='_blank' rel='noreferrer'>Grace Manning</a>
-        </footer>
-    )
-}
--- src/components/PageNotFound/pageNotFound.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function PageNotFound(){
-    return(
-        <div className='container'>
-            oops! that page doesn't exist.
-        </div>
-    )
-}
