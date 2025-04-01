4b626a8687785700030c64c4b100b4567a42f277
Grace Manning <gracebmanning@gmail.com>
2025-03-31 21:15:21 -0700

    project page styling

--- app/projects/[slug]/Project.tsx
+++ app/projects/[slug]/Project.tsx
@@ -1,16 +1,42 @@
 "use client";
 import Link from "next/link";
 import { type SanityDocument, PortableText } from "next-sanity";
+import { MdArrowOutward } from "react-icons/md";
 
 function ProjectPage({ project }: {project: SanityDocument}) {
-    return (
-        <main className="w-full md:m-5 m-0 min-h-screen max-w-3xl p-8 flex flex-col gap-4">
-          <Link href="/projects" className="hover:underline">
+      return(
+        <div className="w-full md:m-5 m-0 flex flex-col justify-center items-center">
+          <Link href="/projects" className="w-[80vw] mb-5 underline hover:text-accent">
             ← Back to projects
           </Link>
-          <h1>{project.title}</h1>
-        </main>
-      );
+          <div className="w-[80vw] flex flex-row justify-between items-center">
+              <h2 className="m-0.5">{project.title}</h2>
+              <div className="flex flex-col">
+                  <h3 className="m-0.5">{project.month} {project.year}</h3>
+                  {project.companyLink === "" ? <h3>{project.company}</h3> : <a className="text-accent hover:underline" href={project.companyLink} target='_blank' rel='noreferrer'><h3>{project.company}</h3></a>}
+              </div>
+          </div>
+          <div className="w-[80vw] flex flex-col mt-7 border-b-1 border-text">
+              <h3>{project.role}</h3>
+              <p className="md:w-full w-[90%]">{project.roleDescription}</p>
+          </div>
+          <div className="w-[80vw] flex md:flex-row flex-col justify-between items-start mt-8">
+              <div className="w-[48%] flex flex-col items-baseline mt-1 mb-1">
+                  <h3>Project Description</h3>
+                  <p className="md:w-full w-[90%]">{project.projectDescription}</p>
+                  <br/>
+                  {project.moreInfoLink === null ? 
+                    null : 
+                    <a className="flex flex-row justify-center items-center mb-1 p-2.5 rounded-3xl bg-accent text-background w-fit duration-200 ease-in-out hover:scale-105" href={project.moreInfoLink} target='_blank' rel='noreferrer'>
+                      more info <MdArrowOutward/>
+                    </a>}
+              </div>
+              <div className="md:w-[50%] w-[90%] flex flex-row justify-center items-center">
+                  {project.embed}
+              </div>
+          </div>
+      </div>
+    )
 }
 
 export default ProjectPage;
