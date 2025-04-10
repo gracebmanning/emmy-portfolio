7e756e11f3399348c606f8fcb37cbc096b1fed8a
Grace Manning <gracebmanning@gmail.com>
2025-04-09 18:39:51 -0700

    video embed UI fixes

--- app/components/embed.tsx
+++ app/components/embed.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
 import { SanityDocument } from "next-sanity";
+import styles from './embed.module.css';
 
 export const EmbedVideo = ({project}:{project:SanityDocument}) => {
   if(project.mediaSource == "videofile"){
     return(
-      <ReactPlayer url={project.videoFile?.asset?.url} width="100%" height="auto" controls />
+      <div className={styles.reactPlayerContainer}>
+        <ReactPlayer 
+          className={styles.reactPlayer}
+          url={project.videoFile?.asset?.url} 
+          width="100%" 
+          height="auto" 
+          controls 
+        />
+      </div>
     )
   } else{
     return(
-      <ReactPlayer url={project.videoLink} width="100%" height="auto" controls />
+      <div className={styles.reactPlayerContainer}>
+        <ReactPlayer
+          url={project.videoLink}
+          className={styles.reactPlayer}
+          width="100%"
+          height="100%"
+          controls
+        />
+      </div>
     )
   }
 }
--- app/projects/[slug]/Project.tsx
+++ app/projects/[slug]/Project.tsx
@@ -2,15 +2,20 @@
 import Link from "next/link";
 import { type SanityDocument } from "next-sanity";
 import { MdArrowOutward } from "react-icons/md";
-import { EmbedVideo } from "@/app/components/embed";
+//import { EmbedVideo } from "@/app/components/embed";
+import dynamic from 'next/dynamic';
+
+const EmbedVideo = dynamic(() => import('@/app/components/embed').then((mod) => mod.EmbedVideo), {
+  ssr: false,
+});
 
 function ProjectPage({ project }: {project: SanityDocument}) {
       return(
-        <div className="w-full md:m-5 m-0 flex flex-col justify-center items-center">
+        <div className="w-full md:m-5 m-0 mb-5 flex flex-col justify-center items-center">
           <Link href="/projects" className="w-[80vw] mb-5 underline hover:text-accent">
             ← Back to projects
           </Link>
-          <div className="w-[80vw] flex flex-row justify-between items-center">
+          <div className="lg:w-[80vw] w-[90vw] flex flex-row justify-between items-center">
               <h2 className="m-0.5">{project.title}</h2>
               <div className="flex flex-col">
                   <h3 className="m-0.5">{project.month} {project.year}</h3>
@@ -21,18 +26,17 @@ function ProjectPage({ project }: {project: SanityDocument}) {
               <h3>{project.role}</h3>
               <p className="md:w-full w-[90%]">{project.roleDescription}</p>
           </div>
-          <div className="w-[80vw] flex md:flex-row flex-col justify-between items-start mt-8">
-              <div className="w-[48%] flex flex-col items-baseline mt-1 mb-1">
+          <div className="w-[80vw] flex lg:flex-row flex-col justify-between items-start mt-8">
+              <div className="lg:w-[48%] w-full flex flex-col items-baseline mt-1 mb-1">
                   <h3>Project Description</h3>
                   <p className="md:w-full w-[90%]">{project.projectDescription}</p>
-                  <br/>
                   {project.moreInfoLink === null ? 
                     null : 
                     <a className="flex flex-row justify-center items-center mb-1 p-2.5 rounded-3xl bg-accent text-background w-fit duration-200 ease-in-out hover:scale-105" href={project.moreInfoLink} target='_blank' rel='noreferrer'>
                       more info <MdArrowOutward/>
                     </a>}
               </div>
-              <div className="md:w-[50%] w-[90%] flex flex-row justify-center items-center">
+              <div className="lg:w-[50%] w-full lg:mt-0 mt-5 flex flex-row justify-center items-center">
                   <EmbedVideo project={project} />
               </div>
           </div>
