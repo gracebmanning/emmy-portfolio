7bfbe9a24c5bc2707b4bf59d7684db0055c53a97
Grace Manning <gracebmanning@gmail.com>
2025-08-19 19:13:37 -0700

    change GM website url

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
     return(
         <footer className="flex flex-row justify-center items-center p-7 border-t-1 border-solid border-text text-text dark:text-white">
-            site created by&nbsp;<Link className="text-accent hover:underline" href='https://grace-manning.com/' target='_blank' rel='noreferrer'>Grace Manning</Link>
+            site created by&nbsp;<Link className="text-accent hover:underline" href='https://graceis.online/' target='_blank' rel='noreferrer'>Grace Manning</Link>
         </footer>
     )
 }
